2acb2f3545d540e672fc8a4c0779130aef1cadc6
yolosolodolo <agamrich@gmail.com>
2023-05-15 22:18:36 -0700

    added enenmy cannon class

--- week13/cannon.py
+++ week13/cannon.py
@@ -123,12 +123,94 @@ class Cannon(GameObject):
     def enemy_cannon(self, user_pos):
         self.set_angle(user_pos)
         return self.strike()
+    
     def move(self, inc):
         '''
         Changes vertical position of the gun.
         '''
         if (self.coord[1] > 30 or inc > 0) and (self.coord[1] < SCREEN_SIZE[1] - 30 or inc < 0):
             self.coord[1] += inc
+
+    def draw(self, screen):
+        '''
+        Draws the gun on the screen.
+        '''
+        
+        gun_shape = []
+        vec_1 = np.array([int(5*np.cos(self.angle - np.pi/2)), int(5*np.sin(self.angle - np.pi/2))])
+        vec_2 = np.array([int(self.pow*np.cos(self.angle)), int(self.pow*np.sin(self.angle))])
+        gun_pos = np.array(self.coord)
+        gun_shape.append((gun_pos + vec_1).tolist())
+        gun_shape.append((gun_pos + vec_1 + vec_2).tolist())
+        gun_shape.append((gun_pos + vec_2 - vec_1).tolist())
+        gun_shape.append((gun_pos - vec_1).tolist())
+        pg.draw.polygon(screen, self.color, gun_shape)
+
+class EnemyCannon(GameObject):
+    '''
+    Cannon class. Manages it's renderring, movement and striking.
+    '''
+    def __init__(self, coord, angle, max_pow, min_pow, color):
+        '''
+        Constructor method. Sets coordinate, direction, minimum and maximum power and color of the gun.
+        '''
+        self.coord = coord
+        self.angle = angle
+        self.max_pow = max_pow
+        self.min_pow = min_pow
+        self.color = color
+        self.active = False
+        self.pow = min_pow
+    
+    def activate(self):
+        '''
+        Activates gun's charge.
+        '''
+        self.active = True
+
+    def gain(self, inc=2):
+        '''
+        Increases current gun charge power.
+        '''
+        if self.active and self.pow < self.max_pow:
+            self.pow += inc
+
+    def strike(self, angle=None):
+        '''
+        Creates ball, according to gun's direction and current charge power.
+        '''
+        if angle is None:
+            angle = self.angle
+
+        # Calculate the velocity of the shell
+        vel = [self.pow * np.cos(np.radians(angle)),
+               -self.pow * np.sin(np.radians(angle))]
+
+        # Create a new shell object
+        new_shell = Shell(list(self.coord), vel, 10)
+
+        # Reset the cannon
+        self.pow = 0
+        self.active = False
+
+    def set_angle(self, target_pos):
+        '''
+        Sets gun's direction to target position.
+        '''
+        self.angle = np.arctan2(target_pos[1] - self.coord[1], target_pos[0] - self.coord[0])
+
+    def enemy_cannon(self, user_pos):
+        self.set_angle(user_pos)
+        return self.strike()
+    
+    def move(self, inc):
+        '''
+        Changes vertical position of the gun.
+        '''
+        '''if (self.coord[1] > 30 or inc > 0) and (self.coord[1] < SCREEN_SIZE[1] - 30 or inc < 0):
+            self.coord[1] += inc
+        doesn't move        
+        '''
 
     def draw(self, screen):
         '''
